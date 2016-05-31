f659f575fbdf2fb38c3caf4e3a0e4f0b59401b83
lepadatu <crsiti1234>
2016-05-31 19:10:17 +0300

    Test

--- demo/servers.groovy
+++ demo/servers.groovy
@@ -2,9 +2,13 @@ def deploy(id) {
     unstash 'war'
     if (id.equals("staging") || id.equals("production") || id.equals("qa")) {
         sh "cp x.war OracleWebLogic/samples/1221-appdeploy-${id}/${id}.war"
+        //If there are already docker images with the same name, delete their corresponding containers and then delete them
+        sh "[ -z \$(docker images -q 1221-appdeploy-${id}) ] || docker rm \$(docker stop \$(docker ps -a -q --filter ancestor=1221-appdeploy-${id})) || \
+            docker rmi 1221-appdeploy-${id}"
+        sh "sleep 5"
         sh "docker build -t 1221-appdeploy-${id} OracleWebLogic/samples/1221-appdeploy-${id}/"
         sh "docker run -d -p 8001:8001 1221-appdeploy-${id}"
-        sleep(5000)
+        sh "sleep 5"
 
     }
 
@@ -12,10 +16,6 @@ def deploy(id) {
 
 def undeploy(id) {
     sh "rm x.war"
-    //If there are already docker images with the same name, delete their corresponding containers and then delete them
-    //sh "[ -z \$(docker images -q 1221-appdeploy-${id}) ] || docker rm \$(docker stop \$(docker ps -a -q --filter ancestor=1221-appdeploy-${id})) || \
-    //        docker rmi 1221-appdeploy-${id}"
-    sleep(5000)
 }
 
 def runWithServer(body) {
